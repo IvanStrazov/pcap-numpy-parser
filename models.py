57c6bd8939a184200be55a7baaa085b7f63182cc
BoxCoxWitnesses <69630874+BoxCoxWitnesses@users.noreply.github.com>
2020-08-16 13:41:21 +0300

    Update models.py

--- models.py
+++ models.py
@@ -3,164 +3,64 @@
 
 
 
-import numpy as np
-import pandas as pd
-import joblib
+DATA_PATH = "data/"
 
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Dropout
+MODEL_BINARY_PATH = "models/binary"
+PREPROC_BINARY_PATH = "preproc/binary"
 
-import config
-
+MODEL_MULTY_PATH = "models/multy/"
+PREPROC_MULTY_PATH = "preproc/multy/"
 
 
-class BinaryClassifier():
-    
-    def __init__(self):
-        """
-        Initialization.
-        """
-        
-        self.preproc_path = config.PREPROC_BINARY_PATH
-        self.model_path = config.MODEL_BINARY_PATH
-        self.create_preprocess()
-        self.create_model()
-        
-        
-    def create_preprocess(self):
-        """
-        Load pipeline to preprocess input data.
-        """
-        
-        self.pipeline = joblib.load(self.preproc_path+"pipeline.pkl")
-        self.pca_2 = joblib.load(self.preproc_path+"pca_2.pkl")
-        self.kmeans_2 = joblib.load(self.preproc_path+"kmeans_2.pkl")
-        self.pca_3 = joblib.load(self.preproc_path+"pca_3.pkl")
-        self.kmeans_3 = joblib.load(self.preproc_path+"kmeans_3.pkl")
-        self.pca_4 = joblib.load(self.preproc_path+"pca_4.pkl")
-        self.kmeans_4 = joblib.load(self.preproc_path+"kmeans_4.pkl")
-        
-    
-    def preprocess(self, X):
-        """
-        Preprocess data.
-        """
-        
-        X = pd.DataFrame(X)
-        X["protocol_type"] = X["protocol_type"].replace(config.protocol_type_dct)
-        X["service"] = X["service"].replace(config.service_dct)
-        X["flag"] = X["flag"].replace(config.flag_dct)
-        
-        return self.pipeline.transform(X)
-    
-    
-    def create_model(self):
-        """
-        Create model and load weights.
-        """
-        
-        self.detector = joblib.load(self.model_path+"model.pkl")
-    
-    
-    def predict(self, X):
-        """
-        Predict attack.
-        
-        Parameters:
-            X (np.array) - feature vector (n, )
-            
-        Returns:
-            y (1/0) - attack
-        """
-        
-        return self.detector.predict(self.preprocess([X]))[0]
+
+display_settings = {
+    "max_columns": 500,
+    "max_rows": 500,
+    "max_colwidth": 500,
+    "expand_frame_repr": True,
+    "max_info_columns": 50,
+    "width": 1000,
+    "precision": 2,
+    "show_dimensions": False
+}
+
+columns = [
+    'duration', 'protocol_type', 'service', 'flag', 'src_bytes',
+    'dst_bytes', 'land', 'wrong_fragment', 'urgent',
+    'serror_rate', 'srv_serror_rate', 'rerror_rate', 
+    'dst_host_serror_rate',
+    'dst_host_srv_serror_rate', 
+    'dst_host_rerror_rate',
+    'dst_host_srv_rerror_rate'
+]
 
 
-    
-class MultyClassifier():
-    """
-    Attack types classifier.
-    """
-    
-    def __init__(self):
-        """
-        Initialization.
-        """
-        
-        self.preproc_path = config.PREPROC_MULTY_PATH
-        self.model_path = config.MODEL_MULTY_PATH
-        self.create_preprocess()
-        self.create_model()
-    
-        
-    def create_preprocess(self):
-        """
-        Load pipeline to preprocess input data.
-        """
-        
-        self.pipeline = joblib.load(self.preproc_path+"pipeline.pkl")
-        self.pca_2 = joblib.load(self.preproc_path+"pca_2.pkl")
-        self.kmeans_2 = joblib.load(self.preproc_path+"kmeans_2.pkl")
-        self.pca_3 = joblib.load(self.preproc_path+"pca_3.pkl")
-        self.kmeans_3 = joblib.load(self.preproc_path+"kmeans_3.pkl")
-        self.pca_4 = joblib.load(self.preproc_path+"pca_4.pkl")
-        self.kmeans_4 = joblib.load(self.preproc_path+"kmeans_4.pkl")
-        
-        
-    def preprocess(self, X):
-        """
-        Preprocess data.
-        """
-        
-        X = pd.DataFrame(X)
-        X["protocol_type"] = X["protocol_type"].replace(config.protocol_type_dct)
-        X["service"] = X["service"].replace(config.service_dct)
-        X["flag"] = X["flag"].replace(config.flag_dct)
-        
-        X = self.pipeline.transform(X)
-        X_cl = self.kmeans_2.transform(self.pca_2.transform(X))
-        X = np.hstack([X, X_cl])
-        X_cl = self.kmeans_3.transform(self.pca_3.transform(X))
-        X = np.hstack([X, X_cl])
-        X_cl = self.kmeans_4.transform(self.pca_4.transform(X))
-        X = np.hstack([X, X_cl])
-        
-        return X
-    
-    
-    def create_model(self):
-        """
-        Create model and load weights.
-        """
-        
-        inp = Input(shape=(105, ), name="inp")
-        dens_1 = Dense(256, activation='relu', name="dens_1")(inp)
-        drop_1 = Dropout(0.7, name="drop_1")(dens_1)
-        dens_2 = Dense(128, activation='sigmoid', name="dens_2")(drop_1)
-        drop_2 = Dropout(0.5, name="drop_2")(dens_2)
-        dens_3 = Dense(64, activation='sigmoid', name="dens_3")(drop_2)
-        drop_3 = Dropout(0.3, name="drop_3")(dens_3)
-        out = Dense(39, activation="softmax", name="out")(drop_3)
-        
-        
-        self.detector = Model(inputs=inp, outputs=out)
-        self.detector.compile(loss="categorical_crossentropy",
-                              optimizer="adam",
-                              metrics=["accuracy"])
-        self.detector.load_weights(self.model_path + "weights/model")
-    
-    
-    def predict(self, X):
-        """
-        Predict type of attack.
-        
-        Parameters:
-            X (np.array) - feature matrix (m, n)
-            
-        Returns:
-            y (np.array) - attack types vector (m, )
-        """
-        
-        return self.detector.predict(self.preprocess(X)).argmax(1)
 
+protocol_type_dct = {5.0: 'tcp',
+                     6.0: 'udp',
+                     10.0: 'icmp'}
+
+service_dct = {20.0: 'ssh', 
+               80.0: 'http',
+               447.0: 'ddm_dfm', 
+               17.0: 'qotd', 
+               55.0: 'isi_gl', 
+               23.0: 'telnet', 
+               43.0: 'whois',
+               323.0: 'immp', 
+               443.0: 'https'}
+
+flag_dct = {1.0: 'S0',
+            2.0: 'S1',
+            3.0: 'SF',
+            4.0: 'S2',
+            5.0: 'S3',
+            6.0: 'RSTO',
+            7.0: 'RSTR',
+            8.0: 'RSTOS0',
+            9.0: 'RSTRH',
+            10.0: 'SH',
+            11.0: 'SHR',
+            0.0: 'OTH',
+            12.0: 'REJ'}
 
